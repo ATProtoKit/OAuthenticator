2af1640477e8811a2b35596f9e8d1c1d6c156c7e
Thomas Rademaker <thomas@radmakr.com>
2026-07-07 09:39:03 -0400

    Fix native macOS build by excluding SwiftUI WebAuthenticationSession (#61)
    * Fix native macOS build by excluding SwiftUI WebAuthenticationSession
    
    * use the _AuthenticationServices_SwiftUI import workaround
    
    ---------
    
    Co-authored-by: Matt <85322+mattmassicotte@users.noreply.github.com>

--- Sources/OAuthenticator/WebAuthenticationSession+Utility.swift
+++ Sources/OAuthenticator/WebAuthenticationSession+Utility.swift
@@ -1,9 +1,11 @@
-#if canImport(AuthenticationServices)
-import AuthenticationServices
+// It is not 100% clear why a plain import of "AuthenticationServices" does not work here,
+// but in some cases/configuration it appears to fail.
+#if canImport(_AuthenticationServices_SwiftUI)
+import _AuthenticationServices_SwiftUI
 import SwiftUI
 
 
-@available(macOS 13.3, iOS 16.4, watchOS 9.4, tvOS 16.4, *)
+@available(macOS 13.3, iOS 16.4, macCatalyst 16.4, watchOS 9.4, tvOS 16.4, *)
 extension WebAuthenticationSession {
 	public func userAuthenticator(preferredBrowserSession: BrowserSession? = nil) -> Authenticator.UserAuthenticator {
 		return {
